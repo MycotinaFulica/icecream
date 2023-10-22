3e854045206509dfd9af88f1f5b95a743b14a5c1
mycotina <>
2023-10-22 19:43:30 +0700

    Add package name and default extension

--- icecream-android/app/src/main/java/org/mycotinafulica/icecream/android/MainActivity.kt
+++ icecream-android/app/src/main/java/org/mycotinafulica/icecream/android/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import org.mycotinafulica.icecream.AIc
+import org.mycotinafulica.icecream.extension.IcDefaultExtension
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,12 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val res = AIc.d()
+        AIc.setTag("MYC")
+        AIc.setAppPackageName("org.mycotinafulica.icecream.android")
+        AIc.addExtension(IcDefaultExtension())
         val dataClass = SomeDataClass(listOf("lst1", "lst2", "lst3"), arrayOf(1, 2, 3))
         val myComplexObj = ComplexClass("myprop1", 5, dataClass)
         AIc.d(myComplexObj)
+
     }
 }
