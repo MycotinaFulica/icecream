753fbcecf8ee78c73a2381f050f4a3f3f9bbb2d6
mycotina <>
2023-10-22 14:34:14 +0700

    Add android version of the library

---
+++ icecream-android/app/src/main/java/org/mycotinafulica/icecream/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.mycotinafulica.icecream
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
